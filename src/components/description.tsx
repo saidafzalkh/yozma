8d3d56c2516785ea8c306f5292a66ceed55a9ac4
Saidafzal (Sayid) Kholkhujaev <saidafzal1209@gmail.com>
2025-07-08 17:18:47 +0500

    Update description.tsx
    added dictionary

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -6,10 +6,23 @@ export default function Description() {
       <h1>Yozma - Personal Web Blog</h1>
       <p>
         <strong>Yozma</strong> is my personal web blog, a space where I share my
-        thoughts, ideas, and discoveries. The name Yozma, an Uzbek word,
-        reflects my passion for writing and expression. Here, you&apos;ll find
-        posts about philosophy, books, technology, and personal
-        insights—everything that fuels my curiosity and learning journey.
+        thoughts, ideas, and discoveries. The name <strong>Yozma</strong>, an Uzbek word,
+        reflects my passion for writing and expression. According to the Uzbek Explanatory Dictionary {' '}
+        <a
+          href="https://izoh.uz/word/yozma"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        izoh.uz
+        </a>, <strong>yozma</strong> means:
+      </p>
+      <blockquote className="italic border-l-4 pl-4 my-2 text-sm text-gray-600">
+        «Yozuv bilan ifodalangan, yozilgan. Og‘zaki va yozma nutq. Yozma manbalar.
+        Yozma til — adabiy til.»
+      </blockquote>
+      <p>
+        Here, you&apos;ll find posts about philosophy, books, technology, and personal
+        insights — everything that fuels my curiosity and learning journey.
         <br />
         <Link href={"/about"}>Read more.</Link>
       </p>
